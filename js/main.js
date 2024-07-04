5325e2f1553d180d6949142333e79e07a7ae4cd9
neoGUN3000 <oluksijsergiiovic@gmail.com>
2024-07-04 18:47:52 -0500

    interface

--- js/main.js
+++ js/main.js
@@ -2,13 +2,31 @@
 
 let nav_buttons = document.getElementsByName('scroll_btn');
 let pages = document.querySelectorAll('.page');
+let heading = document.querySelectorAll('.heading');
 
 
-for(let i = 0; i < nav_buttons.length; i++){
-    nav_buttons[i].addEventListener('click', scrollIntoViewport);
+
+
+
+
+
+
+
+// _____mouse___on____HEADING____
+
+for(let i = 0; i < heading.length; i++){
+  heading[i].addEventListener('mouseover', () => {
+   
+    heading[i].scrollIntoView({ behavior: 'smooth' })} );
+
 }
 
 
+// ______________CLICK__BTN__SCROLL___TO__PAGE___
+
+for(let i = 0; i < nav_buttons.length; i++){
+  nav_buttons[i].addEventListener('click', scrollIntoViewport);
+}
 
 
 
@@ -20,7 +38,15 @@ for(let i = 0; i < nav_buttons.length; i++)
       
     }
 }
+// ____________BLACK____BTN_____WHEN_____PAGE___IS___INTO___VIEW__
+
+// document.addEventListener('DOMContentLoaded', () =>{
+//   window.addEventListener('scroll', () => {
+//     let height_of_page =  document.querySelectorAll('.page');
+//   console.log(height_of_page[1].offsetHeigth.value)
+//   });
+  
+// })
 
 
 
-// pages[i].scrollIntoView

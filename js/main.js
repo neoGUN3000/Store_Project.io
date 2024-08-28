8120b65fb0c90c73df9ff1332db89fb4af9d3b85
neoGUN3000 <oluksijsergiiovic@gmail.com>
2024-08-27 23:50:47 -0500

    main design

--- js/main.js
+++ js/main.js
@@ -1,59 +1,40 @@
 
 
-let gettingBtns = document.querySelectorAll('.side-nav__link');
-let pages = document.querySelectorAll('.page');
-let heading = document.querySelectorAll('.heading');
-const pagesArr = Array.from(pages);
-const headingArr = Array.from(heading);
-const nav_buttons = Array.from(gettingBtns);
+const btnLabel = Array.from(document.querySelectorAll('.r'));
+const pages = Array.from(document.querySelectorAll('.page'));
+const headingArr = Array.from( document.querySelectorAll('.heading'));
+const nav_buttons = Array.from(document.getElementsByName('main-navigation'));
 
 
-
-
-
-window.location.href = nav_buttons[1].getAttribute('href');
-
-
-
-
-
-
-
-
-function ColorChanger(с) {
-  let root = document.documentElement;
-  root.style.setProperty('--main-color-ofDesign', с); 
+const scrollingPage = (e) =>{
+pages[e].scrollIntoView({behavior: 'smooth'});
 }
 
 
 
+const selectedBtn = ()=>{
+for (let i = 0; i < nav_buttons.length; i++) {
+if(nav_buttons[i].checked){
+    btnLabel[i].classList.add('colorGiver');
+    scrollingPage(i);
+     } else{
+          btnLabel[i].classList.remove('colorGiver');
+          } 
 
+  }  
+}
 
-console.log('black');  
-let a = 'rgb(0, 0, 0)';
-let b = 'rgb(42, 42, 42)';
-let c = 'rgb(85, 85, 85)';
-let d = 'rgb(127, 127, 127)';
-let e = 'rgb(170, 170, 170)';
-let f = 'rgb(212, 212, 212)'; 
-let g = 'rgb(250, 250, 250)';
- console.log('white');
-
-headingArr[0].style.color = a;
-
-headingArr[4].style.color = e;
-headingArr[5].style.color = f;
-headingArr[6].style.color = g;
-
-
-
-
-// называть множиство множиством 
+document.addEventListener('DOMContentLoaded', ()=>{
+  btnLabel[0].classList.add('colorGiver');
+  for(let i = 0; i < nav_buttons.length;i++){
+    nav_buttons[i].addEventListener('click', selectedBtn);
+  }
+});
 
 
 
 
-// _____mouse___on____HEADING____
+
 
 
 
@@ -70,9 +51,7 @@ function distanceBetween (element){
 }
 
 
-
-const animatedElement = document.querySelectorAll('.animated');
-const animatedElementArr = Array.from(animatedElement);
+const animatedElementArr = Array.from(document.querySelectorAll('.animated'));
 
 
 window.addEventListener('mouseover', () => {
@@ -101,49 +80,6 @@ window.addEventListener('mouseover', () => {
 
 
 
-
-function visiting_links(){
-  let links = document.querySelectorAll('.side-nav__link') ;
-  links.forEach(link =>{
-    const sectionId = link.getAttribute('href');
-    link.addEventListener('click', function(event){
-      event.preventDefault();
-        window.location.href = sectionId;
- 
-    link.scrollIntoView({behavior: 'smooth'});
-   
-    });
-  });
-  for(let i = 0; i < links.length; i++){
-let isLinkVisited = true;
-let isClassAdded = true;
-
-
-
-
-links[i].addEventListener('click', ()=>{
-    
-   
-if(isLinkVisited){
-  links[i].classList.add('chengeColor');
-  isClassAdded = true;
-}else{
-  links[i].classList.remove('chengeColor');
-  isClassAdded = false;
-}   
-});
-
-//im trying to remove a class from rest of the links arnt pressed
-
-
-
-
-    
-   
-  }
-}
-
-window.addEventListener('DOMContentLoaded', visiting_links);
 
 
 

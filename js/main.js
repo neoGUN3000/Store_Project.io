8a73d74b50fccd7e5375a3617efccda346a93981
neoGUN3000 <oluksijsergiiovic@gmail.com>
2024-09-16 17:30:16 -0500

    present of main design

--- js/main.js
+++ js/main.js
@@ -1,50 +1,59 @@
 
 const btnLabel = Array.from(document.querySelectorAll('.nav__link '));
 const pages = Array.from(document.querySelectorAll('.page'));
-const headingArr = Array.from( document.querySelectorAll('.heading'));
+const heading = Array.from( document.querySelectorAll('.heading'));
 const nav_buttons = Array.from(document.getElementsByName('main-navigation'));
 
-// pages[1].scrollIntoView({behavior: 'smooth'});
 
 
 
 
-const scrollingPage = (e) =>{
-pages[e].scrollIntoView({behavior: 'smooth'});
-}
 
-
-
-const selectedBtn = ()=>{
-for (let i = 0; i < btnLabel.length; i++) {
-if(btnLabel[i].checked){
-  btnLabel[i].classList.add('colorGiver');
-  
-   
-   
-     } if(btnLabel[i].checked  &&  sidePosition(btnLabel[i])){
-      btnLabel[i].classList.add('colorGiverRightSide');
-      scrollingPage(i);
-      
-     }if(btnLabel[i].checked || !sidePosition(btnLabel[i])){
-      btnLabel[i].classList.add('colorGiverBlackBlue');
-      scrollingPage(i);
-      
-     }if(!btnLabel[i].checked){
-      btnLabel[i].classList.remove('colorGiverBlackBlue');
-      btnLabel[i].classList.remove('colorGiverRightSide');
-     }
-     
-          
-        }
-}
 
 document.addEventListener('DOMContentLoaded', ()=>{
-
   btnLabel[0].classList.add('colorGiver');
-  for(let i = 0; i < nav_buttons.length;i++){
-    nav_buttons[i].addEventListener('click', selectedBtn);
+  
+
+const  selectedBtn = (indexBtn)=>{
+    for(let el = 0; el < btnLabel.length; el++){
+      btnLabel[el].classList.remove('colorGiver');
+      indexBtn.classList.add('colorGiver');
+      } 
   }
+
+  
+  
+  for(let i = 0; i < btnLabel.length;i++){
+    btnLabel[i].addEventListener('click', ()=>{
+      selectedBtn(btnLabel[i]);  
+      //to check which btn was pressed add  selected btn colo
+    });
+   
+   
+   
+    // pages[i].addEventListener('mouseover', ()=>{
+    //   let pagesRect = pages[i].getBoundingClientRect();
+    //   let windowsHieght = window.innerHeight;
+    //   let partOfWind = 3;
+    //   if(pagesRect.top < (windowsHieght / partOfWind)){
+    //    for (let index = 0; index < btnLabel.length; index++) {
+    //     btnLabel[index].classList.remove('colorGiver');
+    //    }
+    //    btnLabel[i].classList.add('colorGiver');
+        
+    //   }else console.log('its lower than 50%');
+    // },{passive:true});
+
+  }
+
+
+  
+
+
+
+
+ 
+
 });
 
 
@@ -53,12 +62,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 
-// ____________BLACK____BTN_____WHEN_____PAGE___IS___INTO___VIEW__
+
 function distanceBetween (element){
   const gettingElement = element.getBoundingClientRect();
   const windHieght = window.innerHeight;
   return gettingElement.top < windHieght;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const animatedElementsArr = Array.from(document.querySelectorAll('.animated'));
@@ -77,11 +99,13 @@ for(let i = 0; i < animatedElementsArr.length; i++){
 
 
 
+
+
 window.addEventListener('mouseover', () => {
   let result = distanceBetween(pages[1]);
   let anchor = document.querySelector('.anchor');
 if(result){
-anchor.classList.add('shower');
+   anchor.classList.add('shower');
 }else{anchor.classList.remove('shower');}
 
     
@@ -89,38 +113,6 @@ anchor.classList.add('shower');
 
 
 
-document.addEventListener('', ()=>{
-  let positionTrigger = document.querySelector('#im-special');
-  const ifElementIntoView = (e) =>{
-    let isIntoView = distanceBetween(e);
-    return isIntoView 
-  }
- const result =  ifElementIntoView(positionTrigger);
- let toFixElement = document.querySelector('.container--right');
-if(result){
-toFixElement.style.position = 'static';
-}else toFixElement.style.position = 'fixed';;
-
-
-})
-// this program makes fixed or static position for any containers dependencly on scroll 
-
-
-const sidePosition = (element) =>{
-  let windowWidth = window.innerWidth;
-  let precentage = 60;
-  let elementPosition = element.getBoundingClientRect();
-  let item = element;
- 
-  
-
-   if(elementPosition.left > (precentage / 100) * windowWidth){
-   return true;
-   }else  return false;
- 
-
-  
-}
 
 
 
@@ -131,3 +123,4 @@ const sidePosition = (element) =>{
 
 
 
+7
